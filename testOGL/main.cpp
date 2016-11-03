01f9e9184cabf2d0905bc3464b04c14c9d3237d2
rherrera412 <r.herrera@live.com>
2016-11-03 16:54:00 -0700

    Initial Commit

---
+++ testOGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  testOGL
+//
+//  Created by Ricky Whutever on 11/3/16.
+//  Copyright © 2016 Ricky Herrera. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
