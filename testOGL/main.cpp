451752acb27bf68cc10b6275318ae2062d101e8f
rherrera412 <r.herrera@live.com>
2016-11-03 19:36:28 -0700

    triangle that is now #ed702d

--- testOGL/main.cpp
+++ testOGL/main.cpp
@@ -16,40 +16,36 @@ void render(void){
     glClear(GL_COLOR_BUFFER_BIT);
     /*Initialize the triangle with corresponding RGB */
     glBegin(GL_TRIANGLES);{
-        glColor3f(1.0, 0.0, 0.0);
+        glColor3f(.929, .439, .176);
         glVertex2f(0, 0.5);
-        glColor3f(0.0, 1.0, 0.0);
+        glColor3f(.929, .816, .176);
         glVertex2f(-0.5, -0.5);
-        glColor3f(0.0, 0.0, 1.0);
+        glColor3f(.929, .176, .29);
         glVertex2f(0.5, -0.5);
     }
     glEnd();
-    
 }
+
 
 int main(int argc, const char * argv[]) {
     GLFWwindow *win;
     /*if GLFW not initialize we return an error. */
     if (!glfwInit()){
         return -1;
-        
     }
     win = glfwCreateWindow( 640, 480, "whatsupOpenGL", NULL, NULL);
     if (!win){
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
-    
     if(!glewInit()){
         return -1;
     }
-    
     glfwMakeContextCurrent(win);
     while(!glfwWindowShouldClose(win)){
         render();
         glfwSwapBuffers(win);
         glfwPollEvents();
-        
     }
     glfwTerminate();
     exit(EXIT_SUCCESS);
